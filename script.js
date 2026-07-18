9e873c70db44aa2255d0ef326ea9189ad8db3e4d
Phornpawee26927 <164387@wsk.ac.th>
2026-07-18 22:20:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+// เปิดรูป
+function openImage(src) {
 
+    document.getElementById("popup").style.display = "flex";
+
+    document.getElementById("popup-img").src = src;
+
+}
+
+
+// ปิดรูป
+function closeImage() {
+
+    document.getElementById("popup").style.display = "none";
+
+}
+
+
+// กด ESC เพื่อปิดรูป
+
+document.addEventListener("keydown", function(e){
+
+    if(e.key === "Escape"){
+
+        closeImage();
+
+    }
+
+});
+
+
+// กดพื้นหลังสีดำเพื่อปิดรูป
+
+document.getElementById("popup").addEventListener("click", function(e){
+
+    if(e.target.id === "popup"){
+
+        closeImage();
+
+    }
+
+});
